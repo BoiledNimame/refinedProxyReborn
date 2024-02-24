4ad0489d8a02b495423b0a29a1854d189601e318
HadukiDesktop <haduki.kyouka@gmail.com>
2024-02-25 00:47:51 +0900

    config自体は構造が簡単なのでこれで恐らく完成.

--- src/main/java/com/boilednimame/refinedproxyreborn/config/ServerConfig.java
+++ src/main/java/com/boilednimame/refinedproxyreborn/config/ServerConfig.java
@@ -1,4 +1,47 @@
 package com.boilednimame.refinedproxyreborn.config;
 
+import net.minecraftforge.common.ForgeConfigSpec;
+
+/*
+ * reference:
+ *    https://github.com/refinedmods/refinedstorage/blob/v1.10.6/src/main/java/com/refinedmods/refinedstorage/config/ServerConfig.java
+ */
+
 public class ServerConfig {
+    private final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+    private final ForgeConfigSpec spec;
+    private final RefinedProxy rProxy;
+
+    public ServerConfig() {
+        rProxy = new RefinedProxy();
+
+        spec = builder.build();
+    }
+
+    public RefinedProxy getrRefinedProxy() {
+        return rProxy;
+    }
+
+    public ForgeConfigSpec getSpec() {
+        return spec;
+    }
+
+    public class RefinedProxy {
+        private final ForgeConfigSpec.IntValue usage;
+
+        public RefinedProxy() {
+            builder.push("rProxy");
+
+            usage = builder.comment("The energy used by the RefinedProxy")
+                           .defineInRange("usage", 10, 0, Integer.MAX_VALUE);
+
+            builder.pop();
+        }
+
+        public int getUsage() {
+            return usage.get();
+        }
+
+    }
 }
+
