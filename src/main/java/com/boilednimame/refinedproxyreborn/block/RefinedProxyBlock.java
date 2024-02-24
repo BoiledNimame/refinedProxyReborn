614c91b61f9e9889bbab4c3ab9d565d4a4123485
HadukiDesktop <haduki.kyouka@gmail.com>
2024-02-24 22:59:41 +0900

    ひとまずblockを追加

---
+++ src/main/java/com/boilednimame/refinedproxyreborn/block/RefinedProxyBlock.java
@@ -0,0 +1,10 @@
+package com.boilednimame.refinedproxyreborn.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class RefinedProxyBlock extends Block {
+    public RefinedProxyBlock(Properties properties) {
+        super(properties);
+    }
+
+}
